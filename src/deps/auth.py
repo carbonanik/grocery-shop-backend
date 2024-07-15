38233d12ac6d5be5a18cac223ae76f3b35bce995
Makhdum <makhdum@gmail.com>
2024-07-15 19:46:35 +0600

    commit

--- src/deps/auth.py
+++ src/deps/auth.py
@@ -16,7 +16,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/public/token-x")
 
 
 
